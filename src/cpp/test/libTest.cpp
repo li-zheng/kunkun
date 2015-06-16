98f906fba03fd539590e3be815d1601399c9a150
Zheng Li <lizheng@xiaomi.com>
2015-06-16 17:44:27 +0800

    添加url parser

--- src/cpp/test/libTest.cpp
+++ src/cpp/test/libTest.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "../performance/systemStat.h"
 #include <iostream>
+#include "../url/url_parser.h"
 
 using namespace std;
 
@@ -11,4 +12,28 @@ TEST(LibTest, CPUUsageTest) {
   cout << "cpu usage is " << value << endl;
 
   ASSERT_TRUE(value > 0);
+}
+
+TEST(LibTest, UrlParserTest){
+  struct parsed_url* url = parse_url("http://www.sina.com");
+  
+  ASSERT_FALSE(NULL == url);
+
+  string host(url->host);
+  ASSERT_EQ("www.sina.com", host);
+
+
+  url = parse_url("http://www.sina.com/1");
+
+  ASSERT_FALSE(NULL == url);
+
+  host = (url->host);
+  ASSERT_EQ("www.sina.com", host);
+
+  url = parse_url("http://www.sina.com/abc/");
+
+  ASSERT_FALSE(NULL == url);
+
+  host = (url->host);
+  ASSERT_EQ("www.sina.com", host);
 }
